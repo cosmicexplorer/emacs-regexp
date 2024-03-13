54a1c34f9cd1cc2bbf891ec9ee4acf2ce10e180d
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-03-12 23:16:24 -0400

    begin refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
 /* Description: ???
 
-   Copyright (C) 2024 Danny McClanahan <dmcC2@hypnicjerk.ai>
-   SPDX-License-Identifier: GPL-3.0-or-later
+Copyright (C) 2024 Danny McClanahan <dmcC2@hypnicjerk.ai>
+SPDX-License-Identifier: GPL-3.0-or-later
 
-   This file is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 3 of the
-   License, or (at your option) any later version.
+This file is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
 
-   This file is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
+This file is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 #![warn(rustdoc::missing_crate_level_docs)]
 // #![warn(missing_docs)]
-
 #![deny(unsafe_code)]
 /* Ensure any doctest warnings fails the doctest! */
 #![doc(test(attr(deny(warnings))))]
+
+/* pub struct Literal(pub String); */
+
+pub struct Interval {
+  pub left: usize,
+  pub right: usize,
+}
+
+pub trait Automaton {}
