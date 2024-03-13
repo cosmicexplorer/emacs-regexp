bb16ef706ee0c057141431dc37bd06d997c71060
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-03-13 00:06:26 -0400

    sketch outline

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,103 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 /* Ensure any doctest warnings fails the doctest! */
 #![doc(test(attr(deny(warnings))))]
 
-/* pub struct Literal(pub String); */
+macro_rules! primitive_wrapper {
+  ($name:ident, $inner:ty) => {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    #[repr(transparent)]
+    pub struct $name($inner);
 
-pub struct Interval {
-  pub left: usize,
-  pub right: usize,
+    impl From<$inner> for $name {
+      fn from(i: $inner) -> Self { Self(i) }
+    }
+
+    impl From<$name> for $inner {
+      fn from(n: $name) -> Self { n.0 }
+    }
+  };
 }
 
-pub trait Automaton {}
+pub mod input {
+  pub trait Input {}
+
+  pub struct Bytes;
+  impl Input for Bytes {}
+
+  pub struct Unicode;
+  impl Input for Unicode {}
+}
+
+pub mod chunking {
+  pub trait Chunking {}
+
+  pub struct Contiguous;
+  impl Chunking for Contiguous {}
+
+  pub struct Streaming;
+  impl Chunking for Streaming {}
+}
+
+pub mod interval {
+  pub type Index = u64;
+
+  primitive_wrapper![Offset, Index];
+
+  pub struct Interval {
+    pub left: Offset,
+    pub right: Offset,
+  }
+}
+
+pub mod anchor {
+  pub trait Anchoring {}
+
+  pub struct Double;
+  impl Anchoring for Double {}
+
+  pub struct Left;
+  impl Anchoring for Left {}
+
+  pub struct Right;
+  impl Anchoring for Right {}
+
+  pub struct Unanchored;
+  impl Anchoring for Unanchored {}
+}
+
+pub mod params {
+  pub trait Params {}
+
+  pub struct Single;
+  impl Params for Single {}
+
+  pub struct MultipleParallel;
+  impl Params for MultipleParallel {}
+}
+
+pub mod alphabet {
+  use core::{cmp::Eq, hash::Hash};
+
+  /* use indexmap::IndexSet; */
+
+  pub trait Symbol: Hash+Eq {}
+
+  /* pub struct Alphabet<S>(pub IndexSet<S>) */
+  /* where S: Symbol; */
+}
+
+pub trait Automaton {
+  type A: anchor::Anchoring;
+  type P: params::Params;
+  type S: alphabet::Symbol;
+
+  /* fn invoke(&self) -> A::Out; */
+}
+
+pub trait Builder {
+  fn build<A, S>(alphabet: S) -> A
+  where
+    A: anchor::Anchoring,
+    S: alphabet::Symbol;
+}
+
+/* pub struct Literal(pub String); */
